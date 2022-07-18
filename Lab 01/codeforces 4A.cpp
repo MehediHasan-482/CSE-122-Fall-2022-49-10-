16d1463ced4513f9803b34028fcda6ef97899129
SumaiyaShimu-483 <109482577+SumaiyaShimu-483@users.noreply.github.com>
2022-07-18 23:14:28 +0600

    Add files via upload

---
+++ Lab 01/codeforces 4A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int a;
+cout<<"enter your number:";
+cin>>a;
+if(a%2==0)
+{
+cout<<"yes";
+}else{
+cout<<"no";
+}getch();
+}
